d02b38f88104dc7022387c1dc3f0abba38ef5dc3
yesvik <yesvik@2c8b80e4-04c8-2779-cf7d-f7fd7feb5402>
2010-04-22 10:29:37 +0000

    При включенном eAccelerator'е возникает конфликт из-за одинаковых имен файлов functions/func.common.php и admin/functions/func.common.php поэтому переименовал admin/functions/func.common.php в functions/func.admin.common.php

--- trunk/admin/init.php
+++ trunk/admin/init.php
@@ -8,24 +8,23 @@
  * @filesource
  */
 
-if(defined('ACP'))
-{
-	require(BASE_DIR . '/inc/init.php');
-	require(BASE_DIR . '/class/class.user.php');
-	require(BASE_DIR . '/admin/functions/func.common.php');
-	require(BASE_DIR . '/admin/editor/fckeditor.php');
-
-	$tpl_dir = 'templates/' . (!empty($_SESSION['admin_theme']) ? $_SESSION['admin_theme'] : DEFAULT_ADMIN_THEME_FOLDER);
-	$AVE_Template = new AVE_Template($tpl_dir . '/');
-	$AVE_Template->config_load(BASE_DIR . '/admin/lang/' . (empty($_SESSION['admin_lang']) ? DEFAULT_COUNTRY : addslashes($_SESSION['admin_lang']))  . '/main.txt');
-	$AVE_Template->assign('tpl_dir', $tpl_dir);
-
-	define('SESSION', session_id());
-	$AVE_Template->assign('sess', SESSION);
-}
-else
+if(!defined('ACP'))
 {
 	echo 'Извините, но Вы не имеете права доступа к данному разделу!';
+	exit;
 }
+
+require(BASE_DIR . '/inc/init.php');
+require(BASE_DIR . '/class/class.user.php');
+require(BASE_DIR . '/admin/functions/func.admin.common.php');
+require(BASE_DIR . '/admin/editor/fckeditor.php');
+
+$tpl_dir = 'templates/' . (!empty($_SESSION['admin_theme']) ? $_SESSION['admin_theme'] : DEFAULT_ADMIN_THEME_FOLDER);
+$AVE_Template = new AVE_Template($tpl_dir . '/');
+$AVE_Template->config_load(BASE_DIR . '/admin/lang/' . (empty($_SESSION['admin_lang']) ? DEFAULT_COUNTRY : addslashes($_SESSION['admin_lang']))  . '/main.txt');
+$AVE_Template->assign('tpl_dir', $tpl_dir);
+
+define('SESSION', session_id());
+$AVE_Template->assign('sess', SESSION);
 
 ?>
