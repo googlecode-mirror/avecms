a2201509edb1ac811bed456702d3ae9eacfc4318
yesvik <yesvik@2c8b80e4-04c8-2779-cf7d-f7fd7feb5402>
2010-06-06 21:13:48 +0000

    Забыл выложить config.php новый...

--- trunk/inc/config.php
+++ trunk/inc/config.php
@@ -7,20 +7,56 @@
  * @filesource
  */
 
-$config['mod_rewrite']                = '1';          // {1|0} ЧПУ
-$config['url_suff']                   = '.html';      // суфикс ЧПУ
-$config['default_theme_folder']       = 'ave';        // тема публичной части
-$config['default_admin_theme_folder'] = 'apanel';     // тема панели администратора
-$config['default_language']           = 'ru';         // язык по умолчанию
-$config['session_save_handler']       = true;         // хранить сессии в БД
-$config['session_lifetime']           = 60*24;        // время жизни сессии (Значение по умолчанию 24 минуты)
-$config['cookie_lifetime']            = 60*60*24*14;  // время жизни cookie автологина (60*60*24*14 - 2 недели)
-$config['sql_debug']                  = false;        // {true|false} вывод статистики и списка выполненых запросов
-$config['sql_error']                  = false;        // {true|false} отправка писем с ошибками MySQL
-$config['compile_check']              = false;        // {true|false} контролировать изменения tpl файлов (после настройки сайта установить - false)
-$config['debugging']                  = false;        // {true|false} консоль отладки Smarty
-$config['cache_doc_tpl']              = false;        // {true|false} кэширование скомпилированных шаблонов документов
-$config['cache_lifetime']             = 60*60*24;     // время жизни кэша (60*60*24 - 1 сутки)
-$config['cookie_domain']              = '.ave209.ru'; // имя домена используемое для cookie
+// ЧПУ
+define('REWRITE_MODE', true);
+
+// транслитерация ЧПУ
+define('TRANSLIT_URL', false);
+
+// суфикс ЧПУ
+define('URL_SUFF', '/');
+
+// тема публичной части
+define('DEFAULT_THEME_FOLDER', 'ave');
+
+// тема панели администратора
+define('DEFAULT_ADMIN_THEME_FOLDER', 'apanel');
+
+// язык по умолчанию
+define('DEFAULT_LANGUAGE', 'ru');
+
+// хранить сессии в БД
+define('SESSION_SAVE_HANDLER', true);
+
+// время жизни сессии (Значение по умолчанию 24 минуты)
+define('SESSION_LIFETIME', 60*24*60);
+
+// время жизни cookie автологина (60*60*24*14 - 2 недели)
+define('COOKIE_LIFETIME', 60*60*24*14);
+
+// {true|false} вывод статистики и списка выполненых запросов
+define('PROFILING', true);
+
+// {true|false} отправка писем с ошибками MySQL
+define('SEND_SQL_ERROR', false);
+
+// {true|false} контролировать изменения tpl файлов (после настройки сайта установить - false)
+define('SMARTY_COMPILE_CHECK', true);
+
+// {true|false} консоль отладки Smarty
+define('SMARTY_DEBUGGING', false);
+
+// {true|false} кэширование скомпилированных шаблонов документов
+define('CACHE_DOC_TPL', false);
+
+// время жизни кэша (60*60*24 - 1 сутки)
+define('CACHE_LIFETIME', 60*60*24);
+
+// имя домена используемое для cookie
+//define('COOKIE_DOMAIN', '.ave209.ru');
+
+define('APP_NAME', 'AVE.cms');
+define('APP_VERSION', '2.09RC1');
+define('APP_INFO', APP_NAME . ' ' . APP_VERSION . ' &copy; 2008-2010 <a target="_blank" href="http://www.overdoze.ru/">Overdoze.Ru</a>');
 
 ?>
