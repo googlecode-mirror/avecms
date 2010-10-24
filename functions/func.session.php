49897140b1dd044bd73a358e56adf7bb61829c66
yesvik <yesvik@2c8b80e4-04c8-2779-cf7d-f7fd7feb5402>
2010-10-24 00:36:45 +0000

    Исправил поведение при отсутствии доступа к БД и сделал вывод соответствующих сообщений

--- functions/func.session.php
+++ functions/func.session.php
@@ -25,14 +25,13 @@ function sess_open($save_path, $session_name)
 
 	if (! $sess_dblink = @mysql_connect($config['dbhost'], $config['dbuser'], $config['dbpass']))
 	{
-		echo "<li>Can't connect to ", $config['dbhost'], " as ", $config['dbuser'];
-		echo "<li>MySQL Error: ", mysql_error();
+		display_notice("Error connect MySQL database.");
 		die;
 	}
 
 	if (! @mysql_select_db($config['dbname'], $sess_dblink))
 	{
-		echo "<li>Ќевозможно получить доступ к базе данных ", $config['dbname'];
+		display_notice("Error select MySQL database.");
 		die;
 	}
 
@@ -45,7 +44,7 @@ function sess_close()
 {
 	global $sess_dblink;
 
-	$qid = mysql_query("DELETE FROM ".PREFIX."_sessions WHERE expiry < '".time()."'", $sess_dblink);
+	$qid = @mysql_query("DELETE FROM ".PREFIX."_sessions WHERE expiry < '".time()."'", $sess_dblink);
 
 	return true;
 }
@@ -54,7 +53,7 @@ function sess_read($key)
 {
 	global $sess_dblink;
 
-	$qid = mysql_query("SELECT value, Ip FROM ".PREFIX."_sessions WHERE sesskey = '$key' AND expiry > '".time()."'", $sess_dblink);
+	$qid = @mysql_query("SELECT value, Ip FROM ".PREFIX."_sessions WHERE sesskey = '$key' AND expiry > '".time()."'", $sess_dblink);
 	if ((list($value, $ip) = @mysql_fetch_row($qid)) && $ip == $_SERVER['REMOTE_ADDR']) return $value;
 
 	return false;
@@ -64,9 +63,9 @@ function sess_write($key, $val)
 {
 	global $sess_dblink, $sess_lifetime;
 
-	if (! $qid = mysql_query("INSERT INTO ".PREFIX."_sessions VALUES ('$key', ".(time()+$sess_lifetime).", '".addslashes($val)."', '".$_SERVER['REMOTE_ADDR']."', FROM_UNIXTIME(expiry,'%d.%m.%Y, %H:%i:%s'))", $sess_dblink))
+	if (! $qid = @mysql_query("INSERT INTO ".PREFIX."_sessions VALUES ('$key', ".(time()+$sess_lifetime).", '".addslashes($val)."', '".$_SERVER['REMOTE_ADDR']."', FROM_UNIXTIME(expiry,'%d.%m.%Y, %H:%i:%s'))", $sess_dblink))
 	{
-		$qid = mysql_query("UPDATE ".PREFIX."_sessions SET expiry = ".(time()+$sess_lifetime).", expire_datum = FROM_UNIXTIME(expiry,'%d.%m.%Y, %H:%i:%s'), value = '".addslashes($val)."', Ip = '".$_SERVER['REMOTE_ADDR']."' WHERE sesskey = '".$key."' AND expiry > '".time()."'", $sess_dblink);
+		$qid = @mysql_query("UPDATE ".PREFIX."_sessions SET expiry = ".(time()+$sess_lifetime).", expire_datum = FROM_UNIXTIME(expiry,'%d.%m.%Y, %H:%i:%s'), value = '".addslashes($val)."', Ip = '".$_SERVER['REMOTE_ADDR']."' WHERE sesskey = '".$key."' AND expiry > '".time()."'", $sess_dblink);
 	}
 
 	return $qid;
@@ -76,16 +75,16 @@ function sess_destroy($key)
 {
 	global $sess_dblink;
 
-	return mysql_query("DELETE FROM ".PREFIX."_sessions WHERE sesskey = '$key'", $sess_dblink);
+	return @mysql_query("DELETE FROM ".PREFIX."_sessions WHERE sesskey = '$key'", $sess_dblink);
 }
 
 function sess_gc($maxlifetime)
 {
 	global $sess_dblink;
 
-	$qid = mysql_query("DELETE FROM ".PREFIX."_sessions WHERE expiry < '".time()."'", $sess_dblink);
+	$qid = @mysql_query("DELETE FROM ".PREFIX."_sessions WHERE expiry < '".time()."'", $sess_dblink);
 
-	return mysql_affected_rows($sess_dblink);
+	return @mysql_affected_rows($sess_dblink);
 }
 
 session_set_save_handler("sess_open", "sess_close", "sess_read", "sess_write", "sess_destroy", "sess_gc");
